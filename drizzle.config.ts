2a2e544f868f1e09eed513ddf44457b1da119d09
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-10-24 17:12:58 -0500

    database added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
-import { type Config } from "drizzle-kit";
-
-import { env } from "~/env";
+import { type Config } from "drizzle-kit"
+import { env } from "~/env"
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["drive-tutorial_*"],
-} satisfies Config;
+  dbCredentials: {
+    host: env.SINGLESTORE_HOST as string,
+    port: Number(env.SINGLESTORE_PORT) || 3306,
+    user: env.SINGLESTORE_USER as string,
+    password: env.SINGLESTORE_PASS as string,
+    database: env.SINGLESTORE_DB_NAME as string,
+    ssl: {},
+  },
+} satisfies Config
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,30 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/singlestore"
+import { createPool, type Pool } from "mysql2/promise"
 
-import { env } from "~/env";
-import * as schema from "./schema";
+import { env } from "~/env"
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
-};
+  conn: Pool | undefined
+}
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+const conn =
+  globalForDb.conn ??
+  createPool({
+    host: String(env.SINGLESTORE_HOST),
+    port: Number(env.SINGLESTORE_PORT) || 3306,
+    user: String(env.SINGLESTORE_USER),
+    password: String(env.SINGLESTORE_PASS),
+    database: String(env.SINGLESTORE_DB_NAME),
+    ssl: {},
+    maxIdle: 0,
+  })
+if (env.NODE_ENV !== "production") globalForDb.conn = conn
+// export const client =
+//   globalForDb.client ?? createClient({ url: env.DATABASE_URL })
+// if (env.NODE_ENV !== "production") globalForDb.client = client
 
-export const db = drizzle(client, { schema });
+// export const db = drizzle(client, { schema })
