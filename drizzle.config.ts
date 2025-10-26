b8db20bed789655d434a333ba497818544b2a4f7
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-10-26 17:19:07 -0500

    updated schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { env } from "~/env"
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "singlestore",
-  tablesFilter: ["drive-tutorial_*"],
+  // tablesFilter: ["drive-tutorial_*"],
   dbCredentials: {
     host: env.SINGLESTORE_HOST as string,
     port: Number(env.SINGLESTORE_PORT) || 3306,
