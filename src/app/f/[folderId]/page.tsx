21eed556aae39bc7cc6205b03398756b70360cfa
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-10-27 21:02:10 -0500

    folder id handled via routes

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -4,6 +4,7 @@ import {
   folders as foldersSchema,
 } from "~/server/db/schema"
 import DriveContents from "~/app/drive-contents"
+import { eq } from "drizzle-orm"
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>
@@ -11,13 +12,20 @@ export default async function GoogleDriveClone(props: {
   const params = await props.params
 
   const parsedFolderId = parseInt(params.folderId)
-
+  // degug
+  console.log(parsedFolderId)
   if (isNaN(parsedFolderId)) {
-    console.log(params.folderId)
     return <div>Invalid folder ID</div>
   }
 
-  const files = await db.select().from(filesSchema)
-  const folders = await db.select().from(foldersSchema)
+  const files = await db
+    .select()
+    .from(filesSchema)
+    .where(eq(filesSchema.parent, parsedFolderId))
+
+  const folders = await db
+    .select()
+    .from(foldersSchema)
+    .where(eq(foldersSchema.parent, parsedFolderId))
   return <DriveContents files={files} folders={folders} />
 }
